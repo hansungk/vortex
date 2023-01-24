f8fc305cbd2d8862cf325c004cc7e31640eab256
Hansung Kim <hansung_kim@berkeley.edu>
2023-01-23 18:01:18 -0800

    Add sourceme.sh
    Needed to set env variables for a custom setup where the prebuilt
    binaries are located outside of /opt.

---
+++ sourceme.sh
@@ -0,0 +1,6 @@
+export LLVM_PREFIX=/scratch/hansung/build/vortex-toolchain-prebuilt/llvm-riscv/
+export POCL_CC_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt/pocl/compiler
+export POCL_RT_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt/pocl/runtime
+export VERILATOR_ROOT=/scratch/hansung/build/vortex-toolchain-prebuilt/verilator
+export RISCV_TOOLCHAIN_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt
+export PATH="/scratch/hansung/build/vortex-toolchain-prebuilt/verilator/bin:$PATH"
