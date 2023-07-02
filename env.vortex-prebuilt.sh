f2f1249b931367bc4ff72081b9a895744a4ece7c
Hansung Kim <stephen422@gmail.com>
2023-07-02 13:48:19 -0700

    Make base build directory a variable in script

--- env.vortex-prebuilt.sh
+++ env.vortex-prebuilt.sh
@@ -1,7 +1,8 @@
+BUILDDIR=$HOME/build
 export VORTEX_ENV="vortex-prebuilt"
-export LLVM_PREFIX=/scratch/hansung/build/vortex-toolchain-prebuilt/llvm-riscv/
-export POCL_CC_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt/pocl/compiler
-export POCL_RT_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt/pocl/runtime
-export VERILATOR_ROOT=/scratch/hansung/build/vortex-toolchain-prebuilt/verilator
-export RISCV_TOOLCHAIN_PATH=/scratch/hansung/build/vortex-toolchain-prebuilt
-export PATH="/scratch/hansung/build/vortex-toolchain-prebuilt/verilator/bin:$PATH"
+export LLVM_PREFIX=$BUILDDIR/vortex-toolchain-prebuilt/llvm-riscv/
+export POCL_CC_PATH=$BUILDDIR/vortex-toolchain-prebuilt/pocl/compiler
+export POCL_RT_PATH=$BUILDDIR/vortex-toolchain-prebuilt/pocl/runtime
+export VERILATOR_ROOT=$BUILDDIR/vortex-toolchain-prebuilt/verilator
+export RISCV_TOOLCHAIN_PATH=$BUILDDIR/vortex-toolchain-prebuilt
+export PATH="$BUILDDIR/vortex-toolchain-prebuilt/verilator/bin:$PATH"
