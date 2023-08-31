2fa94b9c2170f1ce4b343e985c44a2a7246e82b7
Hansung Kim <hansung_kim@berkeley.edu>
2023-08-31 14:24:58 -0700

    Hardcode paths into env script instead of args

--- env.vortex-prebuilt.sh
+++ env.vortex-prebuilt.sh
@@ -1,28 +1,17 @@
-if [ $# -lt 1 ]
-then
-    echo "Usage: source env.vortex-prebuilt.sh build-dir"
-    return
-fi
-
 if [ -n "$VORTEX_ENV" ]
 then
     echo "VORTEX_ENV already set. Exiting."
     return
 fi
 
-BUILDDIR=$1
-
-if ! [ -d "$BUILDDIR/vortex-toolchain-prebuilt" ]
-then
-    echo "error: $BUILDDIR/vortex-toolchain-prebuilt does not exist."
-    return 1
-fi
+# PREBUILT_DIR=/scratch/hansung/build/vortex-toolchain-prebuilt-d2ba5df-230831
+PREBUILT_DIR=/scratch/hansung/build/vortex-toolchain-prebuilt-new
 
 export VORTEX_ENV="vortex-prebuilt"
-export LLVM_PREFIX=$BUILDDIR/vortex-toolchain-prebuilt/llvm-riscv/
-export POCL_CC_PATH=$BUILDDIR/vortex-toolchain-prebuilt/pocl/compiler
-export POCL_RT_PATH=$BUILDDIR/vortex-toolchain-prebuilt/pocl/runtime
-export VERILATOR_ROOT=$BUILDDIR/vortex-toolchain-prebuilt/verilator
-export RISCV_TOOLCHAIN_PATH=$BUILDDIR/vortex-toolchain-prebuilt
-export PATH="$BUILDDIR/vortex-toolchain-prebuilt/verilator/bin:$PATH"
+export LLVM_PREFIX=$PREBUILT_DIR/llvm-riscv/
+export POCL_CC_PATH=$PREBUILT_DIR/pocl/compiler
+export POCL_RT_PATH=$PREBUILT_DIR/pocl/runtime
+export VERILATOR_ROOT=$PREBUILT_DIR/verilator
+export RISCV_TOOLCHAIN_PATH=$PREBUILT_DIR/
+export PATH="$BUILDDIR/vortex-toolchain-prebuilt-d2ba5df-230831/verilator/bin:$PATH"
 export PS1="($VORTEX_ENV) $PS1"
