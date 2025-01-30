d59d1e7c91007f45f7c1ea40270eb288c8cbeb7b
Hansung Kim <hansung_kim@berkeley.edu>
2025-01-29 20:23:17 -0800

    Unify filenames in virgo operand/compiler script

--- tests/regression/sgemm_gemmini_dma/compile_hopper.sh
+++ tests/regression/sgemm_gemmini_dma/compile_hopper.sh
@@ -1,11 +1,19 @@
-rm kernel.radiance.elf
-rm -rf binaries
-mkdir binaries
+#!/bin/sh
+
+echo "generating operands"
+
+python3 generate_operands.py
+
 for a in args/*; do
+    echo "compiling GEMM kernel for Virgo with dim ${a}"
     cp -f $a args.bin
     aa=$(basename "$a")
-    cp -f input.a/"$aa" input.a.bin
-    cp -f input.b/"$aa" input.b.bin
-    make > /dev/null
-    mv kernel.radiance.elf binaries/gemmini_hopper_dma"$aa".elf
+    cp -f input.a.rand01.fp16.m${aa}n${aa}k${aa}.row.bin input.a.bin
+    cp -f input.b.rand01.fp16.m${aa}n${aa}k${aa}.row.bin input.b.bin
+
+    # touch source file to force re-building, as the Makefile does not track
+    # binary changes
+    touch kernel.cpp
+
+    make CONFIG=gemm.virgo.hopper.dim${aa}
 done
